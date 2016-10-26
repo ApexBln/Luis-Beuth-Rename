5581ba4bccb333991281854c86c785032099eea4
fragilizer <fragilizer@gmail.com>
2016-10-26 12:32:31 +0200

    Added som functionality
    Added csv-Import/Export-functionality, some GUI tweaking (still ugly)

--- LUIS-Beuth-Rename.py
+++ LUIS-Beuth-Rename.py
@@ -1,35 +1,67 @@
 from tkinter import Tk,Button,Frame,Label,filedialog, ttk
 import os as os
 import tkinter as tkinter
-
+import csv as csv
+#To Do
+"""
+-Studium Generale rein (Liste + CSV), zweites Dropdown
+-If-BA/MA Abfragen zu Studiengängen
+-Main Menü (Datei,Optionen,Info/About) erstellen
+-Öffnen der Ursprungsdatei (Vorschau)
+"""
 class Application(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)        
-        #root.geometry("500x350")
-        root.title("Rename it!")
+
+        root.title("LUIS Beuth Renamer")
         root.attributes('-zoomed', True)
-        self.lst_abschluss = ['Bachelor','Master','Unbekannt']
+        self.lst_abschluss=[]        
+        print("Leer: ",self.lst_abschluss)        
+        self.lst_studiengang=[]
+        self.lst_modul=[]
+        self.lst_semester=[]
+        self.lst_dozent=[]
+        self.lst_przr=[]
+        self.lst_version=[]
+        self.lst_note=[]
+        self.total_content=[]
+        with open('LOUIS-Beuth-Renamer.csv','r',newline='')as csvfile:
+            for row in csv.reader(csvfile):
+                    self.total_content.append(row)
+        self.lst_abschluss=self.total_content[0]
+        self.lst_studiengang=self.total_content[1]
+        self.lst_modul=self.total_content[2]
+        self.lst_semester=self.total_content[4]
+        self.lst_dozent=self.total_content[3]
+        self.lst_przr=self.total_content[5]
+        self.lst_version=self.total_content[6]
+        self.lst_note=self.total_content[7]
+        print(self.lst_abschluss)
+        print(self.lst_studiengang)
+                    
+                    
+
         self.var_abschluss = tkinter.StringVar()
         self.var_abschluss.set("unbekannt")    
-        self.lst_studiengang = ['BA_Bauingenieurwesen','BA_Umweltingenieurwesen-  Bau','BA_Wirtschaftsingenieurwesen- Bau','BA_Geoinformation', 'MA_Wirtschaftsingenieurwesen- Bau', 'MA_Bauingenieurwesen', 'MA_Geoinformation', 'MA_Umweltinformation- GIS', 'Unbekannt']
+
         self.var_studiengang = tkinter.StringVar()
         self.var_studiengang.set("unbekannt")        
-        self.lst_modul = ['modul','Option2','Option3', 'Unbekannt']
+        
         self.var_modul = tkinter.StringVar()
         self.var_modul.set("unbekannt")        
-        self.lst_semester = ['vor2009','SS_09','WS_0910','SS_10','WS_1011','SS_11','WS_1112','SS_12','WS_1213','SS_13','WS_1314','SS_14','WS_1415','SS_15','WS_1516','SS_16','WS_1617','SS_17','WS_1718','SS_18','WS_1819','SS_19','WS_1920','SS_20','WS_2021','SS_21','WS_2122', 'Unbekannt']
+
         self.var_semester = tkinter.StringVar()
         self.var_semester.set("unbekannt")        
-        self.lst_dozent = ['Axmann','Beck','Berger','Bergmann','Breuer','Dick','Domnick','Fehlau','Fischer','Füsser','Gierloff','Hanschke','Hehl','Heider','Heimann','Himburg','Keck','Kickler','Korth','Kramp','Lutz','Meyn','Möller','Nielsen','Pohlmann','Prietz','Resnik','Ripke','Rösler','Schneider','Schomacker','Schweikart','Selle','Stempfhuber','Wagner','Weiß', 'Unbekannt']
+
         self.var_dozent = tkinter.StringVar()
         self.var_dozent.set("unbekannt")        
-        self.lst_przr = ['PRZ_1','PRZ_2', 'Unbekannt']
+
         self.var_przr = tkinter.StringVar()
         self.var_przr.set("unbekannt")        
-        self.lst_version = ['version','Option2','Option3', 'Unbekannt']
+
         self.var_version = tkinter.StringVar()
         self.var_version.set("unbekannt")        
-        self.lst_note = ['1,0','1,3','1,7','2,0','2,3','2,7','3,0','3,3','3,7','4,0','oE', 'Unbekannt']
+
         self.var_note = tkinter.StringVar()
         self.var_note.set("unbekannt")        
         self.create_widgets()
@@ -45,9 +77,9 @@ class Application(Frame):
         self.modul_l = Label(root,text="Modulname:  ")
         self.modul_l.grid(row=2,column=0,sticky='w')
         self.semester_l = Label(root,text="Semester:  ")
-        self.semester_l.grid(row=3,column=0,sticky='w')
+        self.semester_l.grid(row=4,column=0,sticky='w')
         self.dozent_l = Label(root,text="Dozent:  ")
-        self.dozent_l.grid(row=4,column=0,sticky='w')        
+        self.dozent_l.grid(row=3,column=0,sticky='w')        
         self.przr_l = Label(root,text="Prüfungszeitraum:  ")
         self.przr_l.grid(row=5,column=0,sticky='w')        
         self.version_l = Label(root,text="Version:  ")
@@ -55,21 +87,27 @@ class Application(Frame):
         self.note_l = Label(root,text="Note:  ")
         self.note_l.grid(row=7,column=0,sticky='w')
 
-        self.select_button = Button(root,text="Datei",command=self.datei_umbennen)
-        self.select_button.grid(row=8,column=0)
+        self.select_button = Button(root,text="Datei",command=self.datei_waehlen)
+        self.select_button.grid(row=8,column=0,sticky='w')
+        
         self.save_button = Button(root,text="Save",command=self.save_file)
-        self.save_button.grid(row=8,column=1)
+        self.save_button.grid(row=8,column=0,sticky='e')
 
-        self.ausgabe_label = Label(root,text='')
-        self.ausgabe_label.grid(row=9,columnspan=1000)
+        self.vorschau_label = Label(root,text='')
+        self.vorschau_label.grid(row=11,columnspan=1000)
+        self.datei_label = Label(root,text='Datei:  ')
+        self.datei_label.grid(row=9,column=0,sticky='w')
+        self.datei_gewaehlt = Label(root,text='')
+        self.datei_gewaehlt.grid(row=9,column=1)
         
         self.vorschau_button = Button(root,text="Dateiname Vorschau",command=self.dateiname_vorschau)
         self.vorschau_button.grid(row=10,column=0)
         
         self.add_modul_b = Button(root, text="Modul hinzufügen",command=self.add_modul)
         self.add_modul_b.grid(row=2,column=2)
+        
         self.add_dozent_b = Button(root, text="Dozent hinzufügen",command=self.add_dozent)
-        self.add_dozent_b.grid(row=4,column=2)
+        self.add_dozent_b.grid(row=3,column=2)
         
         
     def drop_refresh(self):                             
@@ -91,13 +129,15 @@ class Application(Frame):
         self.drop_note.grid(row=7,column=1)
         
     
-    def datei_umbennen(self):
+    def datei_waehlen(self):
         self.filepath=filedialog.askopenfilename()
         self.filename=os.path.basename(self.filepath)
+        if self.filename != '':
+            self.datei_gewaehlt['text']=self.filename
         
     def dateiname_vorschau(self):
         self.newName = self.var_abschluss.get()+"_"+self.var_studiengang.get()+"_"+self.var_modul.get()+"_"+self.var_semester.get()+"_"+self.var_dozent.get()+"_"+self.var_przr.get()+"_"+self.var_version.get()+"_"+self.var_note.get()+".pdf"
-        self.ausgabe_label['text']=self.newName
+        self.vorschau_label['text']=self.newName
         
     def target_directory(self):
         self.EndPath = self.var_abschluss.get()+"/"+self.var_studiengang.get()+"/"+self.var_modul.get()+"/"+self.var_semester.get()+"/"+self.var_dozent.get()      
@@ -113,6 +153,7 @@ class Application(Frame):
     def add_modul(self):
         eintrag = self.var_modul.get()
         if not eintrag in self.lst_modul:
+            
             self.lst_modul.append(eintrag)
         print(eintrag,self.lst_modul)
         self.drop_refresh()
